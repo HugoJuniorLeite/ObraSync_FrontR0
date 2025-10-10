9e705193f2baf885c369f4f1be9ec8ed796d1e94
Hugo Junior <hjunioreng@gmail.com>
2025-10-10 19:59:34 -0300

    alter poewerbi

--- src/components/dashboard/PowerBIContainer.jsx
+++ src/components/dashboard/PowerBIContainer.jsx
@@ -4,7 +4,7 @@ export default function PowerBIContainer() {
     <div style={{ width: "100%", height: "75vh" }}>
       <iframe
         title="Relatório Power BI"
-        src="https://app.powerbi.com/view?r=eyJrIjoiYzZiNzVjMjgtZjMwOC00OTdjLTljM2ItNWRmMWY4NGViOTY5IiwidCI6ImJkZjMzNTc0LTFiMzUtNDA1Mi05MTg3LTA1MjZmY2M2YTUxZSJ9" 
+        src="https://app.powerbi.com/view?r=eyJrIjoiZjQ1YjcxYWYtNzQxNC00ZDgyLWJhN2QtODJmMjhjOWNkNDM1IiwidCI6ImNkOTI3Nzg5LWFjYzAtNDVmYS1iNDIzLTIyNGNjZGI2NGIzZCJ9"
         style={{ width: "77vw", height: "480px", border: "none" }}
         allowFullScreen={true}
       />
